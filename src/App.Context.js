c4280e28942ae55631fab9c2c9b48aee78d26580
Adrian Suarez <asuarez9382@gmail.com>
2024-03-04 09:48:40 -0800

    adds appContext file

---
+++ src/App.Context.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+
+
+const AnimalContext = React.createContext();
+
+
+
+const MyProvider = ({ children }) => {
+  const [dogList, setDogList] = useState( []); 
+  const [catList, setCatList] = useState([]); 
+
+  useEffect(() => {
+    fetch("http://localhost:5555/pets")
+        .then(res => res.json())
+        .then(petData => {
+            setDogList(petData["dogs"])
+            setCatList(petData["cats"])
+        })
+    }, [])
+
+  return (
+    <AnimalContext.Provider
+        value={ { dogList, setDogList, catList, setCatList } }    
+    >
+        { children }
+    </AnimalContext.Provider>
+  );
+};
+
+export { MyProvider, AnimalContext}; // Exporting the contexts to be used elsewhere
