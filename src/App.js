041b81ccc8a3ac692ce7963c7098e968abd93908
Adrian Suarez <asuarez9382@gmail.com>
2024-03-04 09:47:07 -0800

    adds app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import AdoptACat from "./pages/AdoptACat";
+import AdoptADog from "./pages/AdoptADog";
+import ErrorPage from "./pages/ErrorPage"
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Header />
+        <NavBar /> 
+        <Routes> 
+          <Route path="/" element={<Home />} /> 
+          <Route path="/cats" element={<AdoptACat />} /> 
+          <Route path="/dogs" element={<AdoptADog />} /> 
+          <Route path="*" element={<ErrorPage />} /> 
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
