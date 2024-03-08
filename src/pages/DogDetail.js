9344e7fcd4eabb4718b95884846b1867f51798e3
Adrian Suarez <asuarez9382@gmail.com>
2024-03-08 09:32:44 -0800

    adds useParams to the DogDetail page

--- src/pages/DogDetail.js
+++ src/pages/DogDetail.js
@@ -1,7 +1,9 @@
 import React from "react";
-
+import { useParams } from "react-router-dom"; 
 
 function DogDetail() {
+
+    const params = useParams();
 
     return(
         <h1>Dog Details</h1>
