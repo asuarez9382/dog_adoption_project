04485543cd671f73105a1bd6b0c3db3492578924
Adrian Suarez <asuarez9382@gmail.com>
2024-03-08 10:30:30 -0800

    adds catDetail functionality

--- src/pages/CatDetail.js
+++ src/pages/CatDetail.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
+import { AnimalContext } from "../App.Context";
 
 function CatDetail() {
 
     const {id} = useParams();
+    const {catList} = useContext(AnimalContext)
+
+    const filteredCat = catList.filter(cat => cat.id === parseInt(id))
 
     return(
-        <h1>Cat Details {id}</h1>
+        <div className="animalCard">
+            <h3>{filteredCat[0].name}</h3>
+            <img src={filteredCat[0].image} alt="animal" />
+            <p>Price: ${filteredCat[0].price}</p>
+            <span>{filteredCat[0].isAdopted ? "Adopted" : "Available"}</span>
+        </div>
     )
 }
 
