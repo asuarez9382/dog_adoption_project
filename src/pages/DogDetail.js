baa83923b5a594c1d2c9de757bf61748b8c861b4
Adrian Suarez <asuarez9382@gmail.com>
2024-03-11 09:48:54 -0700

    styles the dog detail page

--- src/pages/DogDetail.js
+++ src/pages/DogDetail.js
@@ -11,16 +11,19 @@ function DogDetail() {
     const filteredDog = dogList.filter(dog => dog.id === parseInt(id))
 
     return(
-        <div className="animalCard">
-            <h3>{filteredDog[0].name}</h3>
-            <img src={filteredDog[0].image} alt="animal" />
-            <p>Price: ${filteredDog[0].price}</p>
-            <p className="pet-paragraph">`{filteredDog[0].name} is a friendly and energetic dog with a playful personality. 
-                {filteredDog[0].name} loves long walks in the park, chasing after tennis balls, and cuddling up with their favorite humans. 
-                With their loyal and affectionate demeanor, 
-                {filteredDog[0].name} makes a wonderful companion for active individuals and families alike`
+        <div className="pet-container">
+            <div className="pet-details">
+                <h3 className="pet-name">{filteredDog[0].name}</h3>
+                <img src={filteredDog[0].image} alt="animal" className="pet-image"/>
+                <h4 className="pet-price">Price: ${filteredDog[0].price}</h4>
+            </div>
+            <p className="pet-paragraph">{filteredDog[0].name} is a friendly and energetic dog with a playful personality. {filteredDog[0].name} loves long walks in the park, chasing after tennis balls, and cuddling up with their favorite humans. 
+                With their loyal and affectionate demeanor, {filteredDog[0].name} makes a wonderful companion for active individuals and families alike
             </p>
         </div>
+
+
+
     )
 }
 
