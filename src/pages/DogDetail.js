0a166e334160fbf7ac0d99345843da0dd560c332
Adrian Suarez <asuarez9382@gmail.com>
2024-03-11 09:22:02 -0700

    adds a paragraph detailing facts about the dogs

--- src/pages/DogDetail.js
+++ src/pages/DogDetail.js
@@ -15,9 +15,15 @@ function DogDetail() {
             <h3>{filteredDog[0].name}</h3>
             <img src={filteredDog[0].image} alt="animal" />
             <p>Price: ${filteredDog[0].price}</p>
-            <span>{filteredDog[0].isAdopted ? "Adopted" : "Available"}</span>
+            <p className="pet-paragraph">`{filteredDog[0].name} is a friendly and energetic dog with a playful personality. 
+                {filteredDog[0].name} loves long walks in the park, chasing after tennis balls, and cuddling up with their favorite humans. 
+                With their loyal and affectionate demeanor, 
+                {filteredDog[0].name} makes a wonderful companion for active individuals and families alike`
+            </p>
         </div>
     )
 }
 
 export default DogDetail;
+
+//Add general details about the dogDetail and catDetail pages
